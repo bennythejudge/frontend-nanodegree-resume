244653b6ddb9cb70af08682d0a6ba712eb42fe67
bennythejudge <logube@gmail.com>
2014-11-01 23:54:00 +0000

    removed unused code from js/helper.js

--- js/helper.js
+++ js/helper.js
@@ -51,7 +51,7 @@ var HTMLschoolName = "<br/><p>%data%<p>";
 var HTMLschoolDegree = "<p>%data%</p>";
 var HTMLschoolDates = "<div class='date-text'><p>%data%</p></div>";
 var HTMLschoolLocation = "<div class='location-text'><p>%data%</p></div>";
-var HTMLschoolMajor = "<em>Major: %data%</em>"
+var HTMLschoolMajor = "<em>Major: %data%</em>";
 
 // online courses
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
@@ -62,35 +62,6 @@ var HTMLonlineURL = "<br><a href='#'>%data%</a>";
 
 // the map
 var googleMap = "<div id='map'></div>";
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-})
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      "x": x,
-      "y": y
-    }
-  );
-  console.log("x location: " + x + "; y location: " + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
